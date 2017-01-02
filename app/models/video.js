dffea9b4f6406c5787ff79f9a97013cd9d23147d
Anna Maria Mathes <mathes@mytum.de>
2017-01-02 22:17:35 +0100

    add funchal, famara, kirkjubaejarklaustur, joekulsarlon, seydisfjoerdur, asbyrgi, hverfjall, drangsnes, porskafjoerdur

--- app/models/video.js
+++ app/models/video.js
@@ -32,7 +32,35 @@ Video.reopenClass({
       guests: '',
       camera: 'Sony Cybershot'
     },
-	{
+    {
+      id: 'funchal',
+      name: 'Funchal',
+      description: 'Madeira',
+      country: 'Portugal',
+      path: '/assets/videos/funchal.mp4',
+      date: '2016-02-15',
+      latitude: 0.0,
+      longitude: 0.0,
+      peopleIn: 'Bine, Anna',
+      peopleOut: 'Stefan',
+      guests: '',
+      camera: 'Sony Cybershot'
+    },
+    {
+      id: 'famara',
+      name: 'Famara',
+      description: 'Lanzarote',
+      country: 'Spain',
+      path: '/assets/videos/famara.mp4',
+      date: '2016-02-17',
+      latitude: 0.0,
+      longitude: 0.0,
+      peopleIn: 'Stefan',
+      peopleOut: 'Bine, Anna',
+      guests: 'Uwe',
+      camera: 'Sony Cybershot'
+    },
+    {
       id: 'reykjavik',
       name: 'Reykjavík',
       description: '',
@@ -46,7 +74,7 @@ Video.reopenClass({
       guests: '',
       camera: 'Sony Alpha 6000'
     },
-	{
+    {
       id: 'gullfoss',
       name: 'Gullfoss',
       description: '',
@@ -60,13 +88,111 @@ Video.reopenClass({
       guests: '',
       camera: 'Sony Alpha 6000'
     },
-	{
+    {
       id: 'landmannahellir',
       name: 'Landmannahellir',
       description: '',
       country: 'Iceland',
       path: '/assets/videos/landmannahellir.mp4',
       date: '2016-08-29',
+      latitude: 0.0,
+      longitude: 0.0,
+      peopleIn: 'Anna',
+      peopleOut: 'Stefan',
+      guests: '',
+      camera: 'Sony Alpha 6000'
+    },
+    {
+      id: 'kirkjubaejarklaustur',
+      name: 'Kirkjubæjarklaustur',
+      description: '',
+      country: 'Iceland',
+      path: '/assets/videos/kirkjubaejarklaustur.mp4'
+      date: '2016-08-30',
+      latitude: 0.0,
+      longitude: 0.0,
+      peopleIn: 'Anna',
+      peopleOut: 'Stefan',
+      guests: '',
+      camera: 'Sony Alpha 6000'
+    },
+    {
+      id: 'joekulsarlon',
+      name: 'Jökulsárlón',
+      description: '',
+      country: 'Iceland',
+      path: '/assets/videos/joekulsarlon.mp4',
+      date: '2016-08-30',
+      latitude: 0.0,
+      longitude: 0.0,
+      peopleIn: 'Stefan',
+      peopleOut: 'Anna',
+      guests: '',
+      camera: 'Sony Alpha 6000'
+    },
+    {
+      id: 'seydisfjoerdur',
+      name: 'Seyðisfjörður',
+      description: '',
+      country: 'Iceland',
+      path: '/assets/videos/seydisfjoerdur.mp4',
+      date: '2016-09-01',
+      latitude: 0.0,
+      longitude: 0.0,
+      peopleIn: 'Anna',
+      peopleOut: 'Stefan',
+      guests: '',
+      camera: 'Sony Alpha 6000'
+    },
+    {
+      id: 'asbyrgi',
+      name: 'Ásbyrgi',
+      description: '',
+      country: 'Iceland',
+      path: '/assets/videos/asbyrgi.mp4',
+      date: '2016-09-02',
+      latitude: 0.0,
+      longitude: 0.0,
+      peopleIn: 'Stefan',
+      peopleOut: 'Anna',
+      guests: '',
+      camera: 'Sony Alpha 6000'
+    },
+    {
+      id: 'hverfjall',
+      name: 'Hverfjall',
+      description: '',
+      country: 'Iceland',
+      path: '/assets/videos/hverfjall.mp4',
+      date: '2016-09-04',
+      latitude: 0.0,
+      longitude: 0.0,
+      peopleIn: 'Anna',
+      peopleOut: 'Stefan',
+      guests: '',
+      camera: 'Sony Alpha 6000'
+    },
+    {
+      id: 'drangsnes',
+      name: 'Drangsnes',
+      description: '',
+      country: 'Iceland',
+      path: '/assets/videos/drangsnes.mp4',
+      date: '2016-09-07',
+      latitude: 0.0,
+      longitude: 0.0,
+      peopleIn: 'Stefan',
+      peopleOut: 'Anna',
+      guests: '',
+      camera: 'Sony Alpha 6000'
+    },
+    {
+      id: 'porskafjoerdur',
+      name: 'Porskafjörður',
+      description: '',
+      country: 'Iceland',
+      path: '/assets/videos/porskafjoerdur.mp4',
+      date: '2016-09-09',
       latitude: 0.0,
       longitude: 0.0,
       peopleIn: 'Anna',
