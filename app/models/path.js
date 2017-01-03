bf3040a4e8493eb97ef3492a37aa54ad65878abc
Anna Maria Mathes <mathes@mytum.de>
2017-01-03 16:55:08 +0100

    add paths: china, sicily, aida, iceland

--- app/models/path.js
+++ app/models/path.js
@@ -15,6 +15,30 @@ Path.reopenClass({
       name: 'Interrail',
       year: '2013',
       videos: ['abisko']
+    },
+    {
+      id: 'china',
+      name: 'China',
+      year: '2014',
+      videos: ['dubai','peking', 'peking2', 'zhangjiajie', 'zhangjiajie_nationalpark', 'guilin', 'tiantouzhai', 'tiantouzhai2', 'yangshuo', 'xingping', 'lijiang', 'tiger_leaping_gorge', 'hongkong']
+    },
+    {
+      id: 'sicily',
+      name: 'Sicily',
+      year: '2015',
+      videos: ['taormina', 'siracusa', 'etna']
+    },
+    {
+      id: 'aida_canary_islands',
+      name: 'AIDA Canary Islands',
+      year: '2016',
+      videos: ['funchal', 'famara', 'puerto_del_rosario', 'santa_cruz', 'las_palmas']
+    },
+    {
+      id: 'iceland',
+      name: 'Iceland',
+      year: '2016',
+      videos: ['reykjavik', 'gullfoss', 'landmannahellir', 'kirkjubaejarklaustur', 'joekulsarlon', 'seydisfjoerdur', 'asbyrgi', 'hverfjall', 'drangsnes', 'porskafjoerdur']
     }
   ]
 });
