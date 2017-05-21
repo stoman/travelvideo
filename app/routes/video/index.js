b0cbe437b64a4a4b58b9f91c76806951cd36b742
Stefan Toman <git@stoman.de>
2017-05-21 06:10:19 +0200

    improve video/index page

--- app/routes/video/index.js
+++ app/routes/video/index.js
@@ -2,6 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.query('video', {});
+    let self = this;
+    //find videos, then group them by country
+    return new Ember.RSVP.Promise(function(resolve/*, reject*/) {
+      self.store.query('video', {sortBy: 'name'}).then(function(videos) {
+        let ret = {};
+        videos.forEach(function(video) {
+          if(!ret.hasOwnProperty(video.get('country'))) {
+            ret[video.get('country')] = {country: video.get('country'), videos: []};
+          }
+          ret[video.get('country')].videos.push(video);
+        });
+        resolve(Object.keys(ret).sort().map(key => ret[key]));
+      });
+    });
   }
 });
