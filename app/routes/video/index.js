8eb805d9cd3992a771fdf7d4a4a44200cfcdb5a3
Stefan Toman <git@stoman.de>
2017-11-19 14:01:22 +0100

    add video and country count to video index page

--- app/routes/video/index.js
+++ app/routes/video/index.js
@@ -7,13 +7,19 @@ export default Ember.Route.extend({
     return new Ember.RSVP.Promise(function(resolve/*, reject*/) {
       self.store.query('video', {sortBy: 'name'}).then(function(videos) {
         let ret = {};
+        let videoCount = 0;
         videos.forEach(function(video) {
+          videoCount++;
           if(!ret.hasOwnProperty(video.get('country'))) {
             ret[video.get('country')] = {country: video.get('country'), videos: []};
           }
           ret[video.get('country')].videos.push(video);
         });
-        resolve(Object.keys(ret).sort().map(key => ret[key]));
+        resolve({
+          videos: Object.keys(ret).sort().map(key => ret[key]),
+          countVideos: videoCount,
+          countCountries: Object.keys(ret).length
+        });
       });
     });
   }
