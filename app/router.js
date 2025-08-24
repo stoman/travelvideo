ce5f061153bb994ef6bb323aa7d0fb679768bb0c
Stefan Toman <git@stoman.de>
2025-08-24 13:12:50 +0200

    Run upgrade codemods using ember-cli-upgrade --run-codemods

--- app/router.js
+++ app/router.js
@@ -4,7 +4,6 @@ import { inject as service } from '@ember/service';
 import EmberRouter from '@ember/routing/router';
 import config from 'travelvideo/config/environment';
 
-<<<<<<< HEAD
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL,
@@ -17,10 +16,10 @@ const Router = EmberRouter.extend({
 
   _trackPage() {
     scheduleOnce('afterRender', this, () => {
-      const page = this.get('url');
+      const page = this.url;
       const title = this.getWithDefault('currentRouteName', 'unknown');
 
-      get(this, 'metrics').trackPage({page, title});
+      this.metrics.trackPage({page, title});
     });
   }
 });
@@ -42,11 +41,3 @@ Router.map(function() {
 });
 
 export default Router;
-=======
-export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
-}
-
-Router.map(function () {});
->>>>>>> 36304b7 (v2.18.2...v3.28.6)
