29ff3e12b485589bdd14fe59c083f40a97f77873
Stefan Toman <git@stoman.de>
2025-11-22 13:57:38 +0100

    Run `lint:fix`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -52,12 +52,7 @@ export default [
    * https://eslint.org/docs/latest/use/configure/ignore
    */
   {
-    ignores: [
-      'dist/',
-      'node_modules/',
-      'coverage/',
-      '!**/.*',
-    ],
+    ignores: ['dist/', 'node_modules/', 'coverage/', '!**/.*'],
   },
   /**
    * https://eslint.org/docs/latest/use/configure/configuration-files#configuring-linter-options
